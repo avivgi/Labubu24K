1d1706ffb3514b3e9e0178276bdecfaf70a41932
avivgi <aviv@gideoni.com>
2026-04-09 15:46:36 +0300

    inter

--- src/bgu/spl/assig1/Scalar.java
+++ src/bgu/spl/assig1/Scalar.java
@@ -1,24 +1,10 @@
 package src.bgu.spl.assig1;
-public abstract class Scalar{
-    public Scalar add(Scalar s){
-        return null;
-    }
-    public Scalar mul(Scalar s){
-        return null;
-    }
-    public Scalar neg(){
-        return null;
-    }
-    public Scalar power(int exponent){
-        return null;
-    }
-    public int sign(){
-        return 0;
-    }
-    public boolean equals(Object o){
-        return false;
-    }
-    public String toString(){
-        return null;
-    }
+public interface Scalar {
+    Scalar add(Scalar s);
+    Scalar mul(Scalar s);
+    Scalar neg();
+    Scalar power(int exponent);
+    int sign();
+    boolean equals(Object o);
+    String toString();
 }
