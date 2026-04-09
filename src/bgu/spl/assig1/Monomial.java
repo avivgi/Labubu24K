c5907b2fb89cc0c3b1bb6cc7aba7d3d1c81b7b95
avivgi <aviv@gideoni.com>
2026-04-09 17:19:54 +0300

    monimial fxix

--- src/bgu/spl/assig1/Monomial.java
+++ src/bgu/spl/assig1/Monomial.java
@@ -1,45 +1,62 @@
 package src.bgu.spl.assig1;
-public class Monomial{
+
+public class Monomial {
     private int exponent;
     private Scalar coefficient;
-    
-    public Monomial(int exponent, Scalar coefficient){
+
+    public Monomial(int exponent, Scalar coefficient) {
         this.exponent = exponent;
         this.coefficient = coefficient;
     }
-    public Monomial add(Monomial m){
-        if(this.exponent == m.exponent){
+
+    public Monomial add(Monomial m) {
+        if (this.exponent == m.exponent) {
             return new Monomial(this.exponent, this.coefficient.add(m.coefficient));
         }
-        return null;
-    } // same exponent, otherwise null
-    public Monomial mul(Monomial m){
+        return null; 
+    }
+
+    public Monomial mul(Monomial m) {
         return new Monomial(this.exponent + m.exponent, this.coefficient.mul(m.coefficient));
     }
-    public Scalar evaluate(Scalar s){
-        return this.coefficient.mul(s.pow(this.exponent));
+
+    public Scalar evaluate(Scalar s) {
+        return this.coefficient.mul(s.power(this.exponent));
     }
-    public Monomial derivative(){
-        if(this.exponent == 0){
-            return new Monomial(0, new Scalar(0));
+
+    public Monomial derivative() {
+        if (this.exponent == 0) {
+            return new Monomial(0, new IntegerScalar(0));
         }
-        return new Monomial(this.exponent - 1, this.coefficient.mul(new Scalar(this.exponent)));
+        Scalar newCoeff = this.coefficient.mul(new IntegerScalar(this.exponent));
+        return new Monomial(this.exponent - 1, newCoeff);
     }
-    public int sign(){
+
+    public int sign() {
         return this.coefficient.sign();
     }
-    public boolean equals(Object o){
-        if(o instanceof Monomial){
-            o = (Monomial) o;
-            return this.exponent == o.exponent && this.coefficient.equals(o.coefficient);
+
+    public boolean equals(Object o) {
+        if (o instanceof Monomial) {
+            Monomial m = (Monomial) o;
+            return this.exponent == m.exponent && this.coefficient.equals(m.coefficient);
         }
         return false;
     }
-    public String toString(){
-        if(exponent == 0){
+
+    public String toString() {
+        if (exponent == 0) {
             return this.coefficient.toString();
         }
-        return this.coefficient.toString() + "x^" + this.exponent;
-    }
+        String coeffStr = this.coefficient.toString();
+        
+        if (coeffStr.equals("1")) coeffStr = "";
+        else if (coeffStr.equals("-1")) coeffStr = "-";
 
+        // Handle exponent of 1 
+        if (exponent == 1) {
+            return coeffStr + "x";
+        }
+        return coeffStr + "x^" + this.exponent;
+    }
 }
