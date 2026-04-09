e3ad0e01cf9a02ffe4681b60d87f55a2ae62f2b9
zivzivo <zivelian122@gmail.com>
2026-04-09 16:34:50 +0300

    finished IntegerScalar

--- src/bgu/spl/assig1/IntegerScalar.java
+++ src/bgu/spl/assig1/IntegerScalar.java
@@ -6,22 +6,25 @@ public class IntegerScalar implements Scalar{
         this.number = number;
     }
     public Scalar add(Scalar s){
-
+        return new IntegerScalar(this.number + ((IntegerScalar)s).number);
     }
     public Scalar mul(Scalar s){
-
+        return new IntegerScalar(this.number * ((IntegerScalar)s).number);
     }
     public Scalar neg(){
-
+        return new IntegerScalar(-this.number);
     }
     public Scalar power(int exponent){
-
+        return new IntegerScalar((int) Math.pow(this.number, exponent));
     }
     public boolean equals(Object o){
-
+        if(o instanceof IntegerScalar){
+            return this.number == ((IntegerScalar)o).number;
+        }        
+        return false;
     }
     public String toString(){
-
+        return number + "";
     }
     public int sign(){
         return Integer.signum(number);
