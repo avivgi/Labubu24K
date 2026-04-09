a40a23278ca43385fc5faad6ef1b7a6d7c713553
zivzivo <zivelian122@gmail.com>
2026-04-09 15:19:59 +0300

    Add abstract Scalar class with basic methods

--- Scalar.java
+++ Scalar.java
@@ -1,1 +1,20 @@
-
+public abstract class Scalar{
+    public Scalar add(Scalar s){
+        return null;
+    }
+    public Scalar mul(Scalar s){
+        return null;
+    }
+    public Scalar neg(){
+        return null;
+    }
+    public Scalar power(int exponent){
+        return null;
+    }
+    public int sign(){
+        return 0;
+    }
+    public boolean equals(Object o){
+        return false;
+    }
+}
